941c579d349c324a9bedbf940acfab97a3a2f072
sunz <sunzroad@gmail.com>
2025-02-14 08:52:00 +0800

    feat: 增加编程相关的Agent

--- docs/agent/data.ts
+++ docs/agent/data.ts
@@ -48,7 +48,7 @@ export const NAV_DATA: NavData[] = [
     ],
   },
   {
-    title: '开源',
+    title: '开源框架',
     items: [
       {
         icon: 'https://fastgpt.cn/logo.svg',
@@ -90,6 +90,76 @@ export const NAV_DATA: NavData[] = [
         title: '扣子',
         desc: '字节出品AI应用构建平台',
         link: 'https://www.coze.cn/',
+      },
+    ],
+  },
+  {
+    title: '编程IDE',
+    items: [
+      {
+        icon: '/icons/cursor.svg',
+        title: 'Cursor',
+        desc: '实力强大的古早（2023）AI编程',
+        link: 'https://www.cursor.com/',
+      },
+      {
+        icon: '/icons/trae.svg',
+        title: 'Trae',
+        desc: 'AI编程工具，免费，目前只有MAC',
+        link: 'https://www.trae.ai/',
+      },
+      {
+        icon: '/icons/windsurf.svg',
+        title: 'Windsurf',
+        desc: 'AI编程工具，付费',
+        link: 'https://codeium.com/windsurf',
+      },
+    ],
+  },
+  {
+    title: '编程AGENT',
+    items: [
+      {
+        icon: '/icons/continue.svg',
+        title: 'Continue',
+        desc: '基于上下文理解，开源，和Cursor的聊天编程类似',
+        link: 'https://www.continue.dev/',
+      },
+      {
+        icon: '/icons/cline.png',
+        title: 'Cline',
+        desc: '基于上下文理解，开源，和Cursor的Composer编程类似，目前只支持VS Code',
+        link: 'https://marketplace.visualstudio.com/items?itemName=saoudrizwan.claude-dev',
+      },
+      {
+        icon: '/icons/v0.svg',
+        title: 'V0',
+        desc: '原型设计转代码',
+        link: 'https://v0.dev/',
+      },
+      {
+        icon: '/icons/tongyi.svg',
+        title: '通义灵码',
+        desc: 'Cursor的替代，聊天+Composer的组合插件',
+        link: 'https://lingma.aliyun.com/',
+      },
+      {
+        icon: '/icons/bolt.svg',
+        title: 'bolt',
+        desc: '一句话出APP，基于web生态的全栈自动化开发',
+        link: 'https://bolt.new/',
+      },
+      {
+        icon: '/icons/openhands.svg',
+        title: 'OpenHands',
+        desc: '一句话AI出应用，开源，Devin平替',
+        link: 'https://all-hands.dev',
+      },
+      {
+        icon: '/icons/devin.png',
+        title: 'Devin',
+        desc: 'AI工程师，很强大，收费',
+        link: 'https://devin.ai/',
       },
     ],
   },
